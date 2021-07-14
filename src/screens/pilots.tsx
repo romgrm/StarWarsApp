3b7fd250ee66162bcb0108f34f2eb77d29a16095
Romain <romaingreaume@gmail.com>
2021-07-14 18:45:54 +0200

    Pass props througth pilots screen | process&structure done

--- src/screens/pilots.tsx
+++ src/screens/pilots.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function Pilots() {
+export default function Pilots({navigation, route}) {
+
+    
     return (
         <View>
-            <Text>Hello there</Text>
+
+            <Text>Hello there {route.params.name}</Text>
+            <Text>Hello there {route.params.height}</Text>
+            <Text>Hello there {route.params.gender}</Text>
         </View>
     )
 }
--- src/screens/starships.tsx
+++ src/screens/starships.tsx
@@ -1,8 +1,13 @@
-import React, { useContext, FC } from "react";
-import { View, Text } from "react-native";
+import React, { useContext, FC, useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 import { PseudoContext } from "../context/pseudoContext";
 import { ContextType } from "../types/contextType";
 import StarshipRequest from "../services/starshipRequest";
+import axios from "react-native-axios";
+import { Starship } from "../interface/starshipsInterface";
+import { CardStarships } from "../components/CardStarships";
+import { Pilots } from "../interface/pilotsInterface";
+import { Card, Button, ListItem } from "react-native-elements";
 
 /**
  * @pseudoPlayer the state present in the pseudoContext
@@ -10,11 +15,61 @@ import StarshipRequest from "../services/starshipRequest";
  */
 export const starships = ({navigation}) => {
   const { pseudoPlayer } = useContext(PseudoContext) as ContextType;
+  const [fetchData, setFetchData] = useState<Starship[]>([]);
+
+  useEffect(() => {
+
+    axios.get("https://swapi.dev/api/starships/").then(async (res) => {
+      const vaisseauxBrut = res.data.results; // data vaisseaux
+      //pour chaque vaisseau
+      const vaisseaux = await axios.all(
+        vaisseauxBrut.map(async (vaisseau) => {
+          const pilotes = await getAllPilotesFrom(vaisseau)
+          vaisseau.pilots = await pilotes;
+          return await vaisseau
+        })
+      )
+      //do something with the vaisseaux
+      await setFetchData(vaisseaux);
+      await console.log(vaisseaux)
+    })
+    
+    
+    const getAllPilotesFrom = async (vaisseau) => {
+      // on remplace tous les url de pilots par le résultat du fetch de l'url,
+      // et on retourne rien tant qu'on les a pas tous récupérés
+      // (l'array de vaisseaux n'est pas modifié ici !)
+      return axios.all(vaisseau.pilots.map(async (url) =>{ 
+        const res = await axios.get(url);
+        return res.data; // pilots data
+      }))
+    }
+    
+  }, []);
+
+  const displayPilotName = (item: Pilots) => {
+    console.log(item)
+    navigation.navigate('Pilots', {name: item['name'], height: item.height, gender: item.gender})
+    // navigation to a new page with item data (perso data)
+  }
 
   return (
     <View>
       <Text>HELLO {pseudoPlayer}</Text>
-      <StarshipRequest props={navigation}/>
+      {fetchData.map((vaisseau, key) => (
+        <Card>
+        <Card.Title>{vaisseau.name}</Card.Title>
+        <Text>{vaisseau.model}</Text>
+        <Text>{vaisseau.manufacturer}</Text>
+        <Text>{vaisseau.cost}</Text>
+        {vaisseau.pilots.map((item) => (
+          <TouchableOpacity onPress={() => displayPilotName(item)}>
+            <ListItem>{item.name}</ListItem>
+          </TouchableOpacity>
+        ))}
+      </Card>
+     
+    ))}
     </View>
   );
 };
